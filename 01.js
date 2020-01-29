24fda19566d3119a627a742c6aa63bceea7562b1
abgonzalez <6ckgwmh41>
2020-01-28 18:35:05 -0600

    Instalacion

---
+++ 01.js
@@ -0,0 +1,11 @@
+const app= new Vue({
+  el:'#app',
+  data: {
+    titulo:'Hola mundo con vue',
+    frutas: [
+      {nombre:'pera', cantidad:10},
+      {nombre:'manzana', cantidad:5},
+      {nombre:'Platano', cantidad:0}
+    ]
+  }
+})
