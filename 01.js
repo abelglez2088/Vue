50b32d94c6084a6436e2f6ce9362c6d1d1f1f298
abgonzalez <6ckgwmh41>
2020-01-29 18:52:12 -0600

    v-model

--- 01.js
+++ 01.js
@@ -6,6 +6,16 @@ const app= new Vue({
       {nombre:'pera', cantidad:10},
       {nombre:'manzana', cantidad:5},
       {nombre:'Platano', cantidad:0}
-    ]
-  }
+    ],
+    nuevaFruta: ''
+
+  },
+    methods: {
+     agregarFruta (){
+        this.frutas.push({
+          nombre: this.nuevaFruta, cantidad:0
+        })
+        this.nuevaFruta=''
+      }
+    }
 })
